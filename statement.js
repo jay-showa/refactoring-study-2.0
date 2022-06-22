6c5114f821dd50eb069d944789c6be29fac0b329
제이.최봉재 <bjchoi@lunasoft.co.kr>
2022-06-22 13:24:19 +0900

    1-4 31p amountFor 함수 추출

--- statement.js
+++ statement.js
@@ -1,3 +1,7 @@
+/*
+1. 기능을 추가하기 쉬운 형태로 수정
+2. 컴파일 - 테스트 - 커밋
+ */
 export const statement = (invoice, plays) => {
   let totalAmount = 0
   let volumeCredits = 0
@@ -10,24 +14,7 @@ export const statement = (invoice, plays) => {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0
-
-    switch (play.type) {
-      case 'tragedy': {
-        thisAmount = 40000
-        if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30)
-        break
-      }
-      case 'comedy': {
-        thisAmount = 30000
-        if (perf.audience > 20) thisAmount += 10000 + 500 * (perf.audience - 20)
-        thisAmount += 300 * perf.audience
-        break
-      }
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`)
-    }
-
+    let thisAmount = amountFor(perf, play)
     volumeCredits += Math.max(perf.audience - 30, 0)
     if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
     result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
@@ -36,6 +23,27 @@ export const statement = (invoice, plays) => {
   result += `총액: ${format(totalAmount / 100)}\n`
   result += `적립 포인트: ${volumeCredits}점\n`
   return result
+}
+
+function amountFor(perf, play){
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy': {
+      thisAmount = 40000
+      if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30)
+      break
+    }
+    case 'comedy': {
+      thisAmount = 30000
+      if (perf.audience > 20) thisAmount += 10000 + 500 * (perf.audience - 20)
+      thisAmount += 300 * perf.audience
+      break
+    }
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`)
+  }
+
+  return thisAmount;
 }
 
 export default statement
