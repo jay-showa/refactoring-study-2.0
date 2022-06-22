98c9d26aaf4998a5afa0cf76533135d80b647d1b
제이.최봉재 <bjchoi@lunasoft.co.kr>
2022-06-22 13:45:58 +0900

    1-4 37p playFor 인라인

--- statement.js
+++ statement.js
@@ -18,37 +18,38 @@ export const statement = (invoice, plays) => {
   }).format
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf)
-    let thisAmount = amountFor(perf, play)
+    let thisAmount = amountFor(perf, playFor(perf))
     volumeCredits += Math.max(perf.audience - 30, 0)
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
+    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
     totalAmount += thisAmount
   }
   result += `총액: ${format(totalAmount / 100)}\n`
   result += `적립 포인트: ${volumeCredits}점\n`
   return result
-}
 
-function amountFor(aPerformance, play){
-  let result = 0;
-  switch (play.type) {
-    case 'tragedy': {
-      result = 40000
-      if (aPerformance.audience > 30) result += 1000 * (aPerformance.audience - 30)
-      break
+
+  function amountFor(aPerformance,play){
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy': {
+        result = 40000
+        if (aPerformance.audience > 30) result += 1000 * (aPerformance.audience - 30)
+        break
+      }
+      case 'comedy': {
+        result = 30000
+        if (aPerformance.audience > 20) result += 10000 + 500 * (aPerformance.audience - 20)
+        result += 300 * aPerformance.audience
+        break
+      }
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
     }
-    case 'comedy': {
-      result = 30000
-      if (aPerformance.audience > 20) result += 10000 + 500 * (aPerformance.audience - 20)
-      result += 300 * aPerformance.audience
-      break
-    }
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`)
+
+    return result;
   }
-
-  return result;
 }
+
 
 export default statement
