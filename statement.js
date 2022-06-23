ba8c2494da30cb04cdc5331ec775b0f0c2520841
Sebastian <dgseo@lunasoft.co.kr>
2022-06-23 13:22:13 +0900

    p.42 format 함수 추출

--- statement.js
+++ statement.js
@@ -18,14 +18,18 @@ export const statement = (invoice, plays) => {
     return result;
   }
 
+  function format(aNumber){
+
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
+  }
+
   let totalAmount = 0
   let volumeCredits = 0
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditFor(perf);
