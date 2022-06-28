fd304d462646f135588340571749a08ce8e3fbc6
kkl0308 <kkl0308@naver.com>
2022-06-28 13:46:15 +0900

    p56. playfor 이동 및 호출변경

--- statement.js
+++ statement.js
@@ -13,7 +13,13 @@ export const statement = (invoice, plays) => {
   function enrichPerformance(aPerformance)
   {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    
     return result;
+  }
+
+  function playFor(aPerformance){
+    return plays[aPerformance.playID];
   }
 }
 
@@ -22,22 +28,18 @@ function renderPlainText(data, plays){
   let result = `청구 내역 (고객명: ${data.customer})\n`
 
   for (let perf of data.performances) {
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
   }
 
   result += `총액: ${usd(totalAmount())}\n`
   result += `적립 포인트: ${totalVolumeCredits()}점\n`
-  return result
-
-  function playFor(aPerformance){
-    return plays[aPerformance.playID];
-  }
+  return result;
 
   function volumeCreditFor(aPerformance){
     let result = 0
     
     result += Math.max(aPerformance.audience - 30, 0)
-    if (playFor(aPerformance).type === 'comedy') 
+    if (aPerformance.play.type === 'comedy') 
       result += Math.floor(aPerformance.audience / 5)
 
     return result;
@@ -72,7 +74,7 @@ function renderPlainText(data, plays){
   }
   function amountFor(aPerformance){
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy': {
         result = 40000
         if (aPerformance.audience > 30) result += 1000 * (aPerformance.audience - 30)
@@ -85,7 +87,7 @@ function renderPlainText(data, plays){
         break
       }
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
     }
 
     return result;
