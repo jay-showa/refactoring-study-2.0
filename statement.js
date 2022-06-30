5d2c0f2d122fd44b7219802d9130c583297ee5fb
Bell <bell@Bellui-MacBookAir.local>
2022-06-30 13:33:00 +0900

    p.59 중간 데이터 생성 전달 추가

--- statement.js
+++ statement.js
@@ -4,13 +4,16 @@
  */
 
 export const statement = (invoice, plays) => {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
 
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
