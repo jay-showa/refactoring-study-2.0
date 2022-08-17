083f89ee22fd4a443445c83a7811d4997ef0b0d5
Two-Bell <jlee@lunasoft.co.kr>
2022-08-17 13:43:25 +0900

    chapter[6-2] init

---
+++ chapter6/2_inline_function.js
@@ -0,0 +1,11 @@
+const aDriver = {
+  numberOfLateDeliveries: 0,
+};
+
+function rating(aDriver) {
+  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(aDriver) {
+  return aDriver.numberOfLateDeliveries > 5;
+}
